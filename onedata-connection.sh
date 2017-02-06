991d6af504501747d92238cbd10c885be7abc485
mtangaro <ma.tangaro@gmail.com>
2017-02-06 15:46:56 +0000

    first commit for onedata connection script

---
+++ onedata-connection.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export ONECLIENT_AUTHORIZATION_TOKEN=MDAxNWxvY2F00aW9uIG9uZXpvbmUKMDAzYmlkZW500aWZpZXIgbVVfYjRpZUt4WGZtbVBWMG0000QjhURzNPUEJhdVJFS3NybGduU00ZGYWkwbwowMDFhY2lkIHRpbWUgPCAxNTEzOTQxMjc3CjAwMmZzaWduYXR1cmUgNZ00sfBXZuuS3Q7I00A02qNe4rWw6lAWPJdYpor46u6Q7AK
+
+export PROVIDER_HOSTNAME=cdmi-indigo.recas.ba.infn.it
+
+oneclient --authentication token /refdata
+

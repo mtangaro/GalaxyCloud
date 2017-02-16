08662d94da742986e50364840994f9d750e6ec66
mtangaro <ma.tangaro@gmail.com>
2017-02-16 09:51:07 +0000

    separate galaxy functions

--- galaxyctl.sh
+++ galaxyctl.sh
@@ -36,7 +36,7 @@ _fail=" [$red FAIL $none]"
 
 
 ################################################################################
-# GALAXY FUNCTIONS
+# GALAXY
 
 IP="90.147.102.96"
 URL="http://${IP}/galaxy"
@@ -47,309 +47,26 @@ supervisord_conf_file=${supervisord_conf_path}/supervisord.conf
 
 uwsgi_pidfile=/var/log/galaxy/uwsgi-master.pid
 
+galaxyctl_functions_path=/home/galaxy/galaxycloud-testing
+source ${galaxyctl_functions_path}/galaxyctl_functions
 
-#____________________________________
-# Load Galaxy environment
-
-function __load_galaxy_env(){
-  echo -ne "\nLoading Galaxy virtual environment: "
-  cd /home/galaxy/galaxy
-  . /home/galaxy/galaxy/.venv/bin/activate
-  echo -ne "${_ok}\n"
-}
-
-#____________________________________
-# Check if supervisord is running
-# Following the Unix convention of expecting utilities to return zero for success and non-zero for failure, so boolean conditions are inverted.
-
-function __check_supervisord(){
-  if ps ax | grep -v grep | grep supervisord > /dev/null
-  then
-    return 0
-  else
-    return 1
-  fi
-}
-
-#____________________________________
-# Check supervisord status
-
-function __supervisord_status(){
-  __check_supervisord &> /dev/null
-  code=$?
-  if [ $code -eq "0" ]; then
-    echo -e "\nSupervisord service: ${_ok}"
-    return 0
-  else
-    echo -e "\nSupervisord service: ${_fail}."
-    return 1
-  fi
-}
-
-#____________________________________
-# Check if Galaxy instance by curl
-
-function __galaxy_curl(){
-  if curl -s --head  --request GET ${URL} | grep "200 OK" > /dev/null
-  then
-    return 0
-  else
-    return 1
-  fi
-}
-
-#____________________________________
-# Show galaxy status
-
-function __galaxy_url_status(){
-  __galaxy_curl &> /dev/null
-  code=$?
-  if [ $code -eq "0" ]; then 
-    echo -e "\nGalaxy server on-line: ${_ok}"
-  else
-    echo -e "\nGalaxy server on-line: ${_fail}"
-  fi
-}
-
-#____________________________________
-# Wait Galaxy for 5 minuts, then give "connection timeout error"
-
-function __wait_galaxy(){
-  end=$((SECONDS+300)) # wait 5 minutes before exit
-
-  __galaxy_curl &> /dev/null
-  code=$?
-  if $DEBUG; then echo "First code: $code"; fi
-  
-  if [ $code -eq "1" ]; then
-    until [[ $code -eq "0" ]]; do
-      sleep 0.1
-      if $DEBUG; then echo "calling curl..."; fi
-      __galaxy_curl &> /dev/null
-      code=$? # update code var
-      if $DEBUG; then echo "code: $code"; fi
-      if [ $SECONDS -gt $end ]; then
-        echo -e "\nConnection time out. Galaxy server on-line: ${_fail}"
-        return 1
-      fi
-    done
-    __galaxy_url_status
-    return 0
-  else
-    __galaxy_url_status
-    return 0
-  fi
-}
-
-#____________________________________
-# Display supervisorctl status output for Galaxy
-
-function __galaxy_server_status(){
-  supervisorctl status galaxy:
-}
-
-#____________________________________
-# Display uwsgi ps output
-
-function __galaxy_ps(){
-  ps -aux | grep "[u]wsgi" # brackets needed to avoid grep showing itself
-}
-
-#____________________________________
-# This function restart galaxy if the uWSGI master node is in "S" state. Needs uwsgi master node pid file to work.
-
-function __check_uwsgi_master_status(){
-  stat=$(ps -o stat --no-headers -p $(cat ${uwsgi_pidfile}))
-  if [ "$stat" != "Sl" ]; then
-    __restart_galaxy
-  fi
-}
-
-#____________________________________
-# Show Galaxy server status
-
-function __galaxy_status(){
-  __galaxy_url_status
-  echo -e "\nSupervisorctl status:"; __galaxy_server_status
-  echo -e "\nuWSGI status:"; __galaxy_ps
-}
-
-#____________________________________
-# Define start function
-
-function __start_galaxy(){
-  __load_galaxy_env
-
-  echo -e "\nStarting Galaxy..."
-
-  if __check_supervisord ; then
-    supervisorctl start galaxy:
-  else
-    /usr/bin/supervisord -c $supervisord_conf_file
-  fi
-  __wait_galaxy
-}
-
-#____________________________________
-# Define stop function
-
-function __stop_galaxy(){
-  __load_galaxy_env
-
-  echo -e "\nStopping Galaxy using supervisord:\n"
-  supervisorctl stop galaxy:
-
-  echo -e "\nuWSGI nodes check: "
-  if [ "$(pidof uwsgi)" ]; then
-    # processes found
-    echo -ne "Killing uwsgi residual nodes: "
-    kill -9 $(pidof uwsgi)
-    echo -ne "${_ok}\n"
-  else
-  # process not found
-  echo -ne "uWSGI graceful stop: ${_ok}\n"
-  fi
-}
-
-#____________________________________
-# Define restart function
-
-function __restart_galaxy(){
-  echo -e "\nRestarting the Galaxy production environment"
-  __stop_galaxy
-  __start_galaxy
-}
-
-#____________________________________
-# Show Galaxy help
-
-function __galaxy_help(){
-  echo -e "\nUsage: galaxyctl galaxy <option>"
-  echo -e "\nGalaxy options:\n"
-  echo -e '  - help [print-out Galaxy options]\n'
-  echo -e '  - start [start the Galaxy server]\n'
-  echo -e '  - stop [stop the Galaxy server]\n'
-  echo -e '  - restart [restart the Galaxy server]\n'
-  echo -e '  - status [check the status of the whole service]\n'
-  echo -e '  - on-line [check if Galaxy is Up and Running]\n'
-  echo -e '  - ps [check uWSGI processes with ps]\n'
-  echo -e '  - load-env [load Galaxy virtual environment]\n'
-}
-
-#____________________________________
-# Galaxy options
-
-if [ "$1" == "galaxy" ]; then
-  if [ "$2" == "start" ]; then __start_galaxy; fi
-  if [ "$2" == "stop" ]; then __stop_galaxy; fi
-  if [ "$2" == "restart" ]; then __restart_galaxy; fi
-  if [ "$2" == "status" ]; then __galaxy_status; fi
-  if [ "$2" == "online" ]; then __galaxy_url_status; fi
-  if [ "$2" == "ps" ]; then __galaxy_ps; fi
-  if [ "$2" == "load-env" ]; then __load_galaxy_env; fi
-  if [ "$2" == "help" ]; then __galaxy_help; fi
-fi
 
 
 ################################################################################
-# STORAGE FUNCTIONS
+# LUKS
 
-cryptdev_conf_file='/etc/luks-cryptdev.conf'
+luks_script_path=/home/galaxy/galaxycloud-testing
 
-#____________________________________
-# Display dmsetup info
-
-function __dmsetup_info(){
-  dmsetup info /dev/mapper/${CRYPTDEV}
-}
-
-#____________________________________
-# check encrypted storage mounted
-function __cryptdev_status(){
-  __dmsetup_info &>/dev/null
-  if [ $? -eq 0 ]; then
-    echo -e "\nEncrypted volume: ${_ok}"
-  else
-    echo -e "\nEncrypted volume: ${_fail}"
-  fi
-}
-
-#____________________________________
-# luksOpen device
-
-function __luksopen_cryptdev(){
-  cryptsetup luksOpen /dev/disk/by-uuid/${UUID} ${CRYPTDEV}
-  dmsetup info /dev/mapper/${CRYPTDEV}
-  mount /dev/mapper/${CRYPTDEV} $MOUNTPOINT
-  code=$?
-  if [ "$code" -ne 0 ]; then
-    return 31 # return error code 0
-  else 
-    chown galaxy:galaxy $MOUNTPOINT
-    return 0 # return success
-  fi
-}
-
-#____________________________________
-# Open encrypted device
-
-function __cryptdev_open(){
-  __luksopen_cryptdev
-  code=$?
-  if [ "$code" -eq "0" ]; then
-    __cryptdev_status
-  else
-    echo -e "\nEncrypted volume mount: ${_fail}"
-  fi
-}
-
-#____________________________________
-# luksClose device 
-
-function __luksclose_cryptdev(){
-  umount $MOUNTPOINT
-  cryptsetup close ${CRYPTDEV}
-}
-
-#____________________________________
-# Close encrypted device
-
-function __cryptdev_close(){
-  __luksclose_cryptdev
-  __dmsetup_info &>/dev/null
-  if [ $? -eq 0 ]; then
-    echo -e "\nEncrypted volume umount: ${_fail}"
-  else
-    echo -e "\nEncrypted volume umount: ${_ok}"
-  fi
-}
-
-#____________________________________
-# Show Galaxy help
-
-function __cryptdev_help(){
-  echo -e "\nUsage: galaxyctl cryptdevice <option>"
-  echo -e "\nEncrypted volume options:\n"
-  echo -e "  - help [print-out cryptdevice options]\n"
-  echo -e '  - open [luks open and mount volume]\n'
-  echo -e '  - close [luks close and umount volume]\n'
-  echo -e '  - status [check volume status]\n'
-}
-
-#____________________________________
-# Cryptdevice options
-
-if [ "$1" == cryptdevice ]; then
-  source $cryptdev_conf_file
-  if [ "$2" == 'open' ]; then __cryptdev_open; fi
-  if [ "$2" == 'close' ]; then __cryptdev_close; fi
-  if [ "$2" == 'status' ]; then __cryptdev_status; fi
-  if [ "$2" == 'help' ]; then __cryptdev_help; fi
-fi
+source ${luks_script_path}/luksctl.sh
 
 
 ################################################################################
-# PRODUCTION ENVIRONMENT FUNCTION
+# ONEDATA
+
+
+
+################################################################################
+# PRODUCTION ENVIRONMENT SECTION
 
 #____________________________________
 # Print out intro banner
@@ -384,7 +101,9 @@ function __help(){
 # Galaxy production environment initialisation
 
 function __init(){
+
   # Encrypted volume section
+  # ========================
 
   __dmsetup_info &>/dev/null
   if [ $? -eq 0 ]; then
@@ -397,9 +116,11 @@ function __init(){
 
   
   # Onedata section
+  # ===============
 
   
   # Galaxy section
+  # ==============
 
   __galaxy_curl &> /dev/null
   code=$?

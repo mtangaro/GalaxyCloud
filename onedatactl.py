540d6fe92ffc20dff27b0bdafbd5590d5c4cdfe7
mtangaro <galaxy@galaxy-test.cloud.ba.infn.it>
2017-05-14 18:35:03 +0000

    add options

--- onedatactl.py
+++ onedatactl.py
@@ -4,15 +4,32 @@ Author: Marco Tangaro
 mail: ma.tangaro@ibbe.cnr.it
 '''
 
+import os
 import argparse
 import subprocess
 
 try:
-    import ConfigParser as configparser
+  import ConfigParser
 except ImportError:
-    import configparser
+  import configparser
 
 fdefault = '/etc/galaxy/onedatactl.ini'
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# ok and fail variables
+_ok = '[ ' + bcolors.OKGREEN + 'OK' + bcolors.ENDC + ' ]'
+_stop = '[ STOP ]'
+_fail = '[ ' + bcolors.FAIL + 'FAIL' + bcolors.ENDC + ' ]'
+
 
 #______________________________________
 def cli_options():
@@ -49,14 +66,45 @@ def check_ini_file(fname):
      return False
 
 #______________________________________
+def check_configuration(fname):
+  par = read_ini_file(fname, 'refdata')
+  if par:
+    print par[0]
+    print par[1]
+    print par[2]
+  upar = read_ini_file(fname, 'userdata')
+  if upar:
+    print upar[0]
+    print upar[1]
+    print upar[2]
+
+
+#______________________________________
 def read_ini_file(fname, section):
   configParser = ConfigParser.RawConfigParser()
   configParser.readfp(open(fname))
   configParser.read(fname)
-  provider = configParser.get(section , 'provider')
-  token = configParser.get(section , 'token')
-  mountpoint = configParser.get(section , 'mountpoint')
-  params = [provider, token, mountpoint]
+
+  params = []
+
+  if configParser.has_option(section, 'mountpoint'): 
+    params.append(configParser.get(section , 'mountpoint'))
+  else:
+    print 'Section [' + section + '] not enabled!'
+    return False
+
+  if configParser.has_option(section, 'provider'):
+    params.append(configParser.get(section , 'provider'))
+  else:
+    print 'No provider specified. Check you configuration file: ' +_fail
+    return
+
+  if configParser.has_option(section, 'token'):
+    params.append(configParser.get(section , 'token'))
+  else:
+    print 'No token specified. Check your configuration file:' + _fail
+    return
+
   return params
 
 
@@ -80,14 +128,32 @@ def load_from_file(fname, refdata, userdata):
     if userdata: load_data(fname, 'USERDATA')
 
   elif check_ini_file(fdefault):
-    print 'Loading default options from' + fdefault
+    print 'Unable to find configuration file. Loading default options from' + fdefault
     if refdata: load_data(fdefault, 'REFDATA')
     if userdata: load_data(fdefault, 'USERDATA')
 
   else:
-    print 'Unable to load configuration file. Please load Onedata parameters manually'
+    print 'Unable to load configuration file. Please check your configuration or load Onedata parameters manually!'
     return
 
+#______________________________________
+def load_config(fname, data):
+  if check_ini_file(fname):
+    load_data(fname, data)
+
+  elif check_ini_file(fdefault):
+    print 'Unable to find configuration file. Loading default options from' + fdefault
+    load_data(fdefault, data)
+
+  else:
+    print 'Unable to load configuration file. Please check your configuration or load Onedata parameters manually!'
+    return
+
+
+#______________________________________
+def check_mountpoint(data):
+  par = read_ini_file(fdefault, data)
+  return os.path.ismount(par[2])
 
 #______________________________________
 def onedatactl():
@@ -96,14 +162,29 @@ def onedatactl():
 
   check_oneclient()
 
-  if options.config_file:
-    load_from_file(options.config_file, options.refdata, options.userdata)
-  else:
-    if options.refdata: connect(options.provider, options.token, options.mountpoint)
-    if options.userdata: connect(options.provider, options.token, options.mountpoint)
+  check_configuration(options.config_file)
+
+  #---
+  # Reference data section
+#  if 
+#
+#  if options.config_file:
+#    load_from_file(options.config_file, options.refdata, options.userdata)
+#  else:
+#    if options.refdata: connect(options.provider, options.token, options.mountpoint)
+#    if options.userdata: connect(options.provider, options.token, options.mountpoint)
+
+
+#  # Mount volumes
+#  if options.config_file:
+#    load_from_file(options.config_file, options.refdata, options.userdata)
+#  else:
+#    if options.refdata: connect(options.provider, options.token, options.mountpoint)
+#    if options.userdata: connect(options.provider, options.token, options.mountpoint)
 
 
   # check if volume is mounted
+  #print check_mountpoint('REFDATA')
   # if oneclient or rest api option is not there, check with fuse if a volume is mounted, then check for referencee data
 
 
